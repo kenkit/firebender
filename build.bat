0621515c6a17131224e12b9a540d5370b867db20
Your Cosmo <kkituyi@yahoo.com>
2017-11-22 20:28:37 +0300

    fix msbuild

--- build.bat
+++ build.bat
@@ -23,7 +23,6 @@ cd %CURL%
 mkdir build
 cd build
 cmake ..  -DCMAKE_INSTALL_PREFIX:PATH=%CURL%\build\Release
-msbuild curl.sln /p:Configuration=Debug p:/Platform=x86
 msbuild curl.sln /p:Configuration=Debug /p:Platform="Win32"
 cp  %CURL%\build\Release\bin\libcurl.dll %PROJECT_DIR%\Debug
 set CURL=%CURL%\build\Release\
